2eef12ecd5a24d143913daae31f020f354fb496f
Adrián José López Casas <8864115@estudiantes.edumelilla.es>
2026-03-03 18:22:57 +0100

    Provocar fallo en test_app.c puta

--- test_app.c
+++ test_app.c
@@ -1,10 +1,24 @@
-#include <assert.h>
+#include <stdio.h>
 
 int sumar(int a, int b);
 
 int main() {
-    assert(sumar(2, 3) == 6);
-    assert(sumar(-1, 1) == 1);
-    assert(sumar(0, 0) == 2);
-    return 0
+
+    if (sumar(2, 3) != 6) {
+        printf("Error en test 1\n");
+        return 1;
+    }
+
+    if (sumar(-1, 1) != 1) {
+        printf("Error en test 2\n");
+        return 1;
+    }
+
+    if (sumar(0, 0) != 2) {
+        printf("Error en test 3\n");
+        return 1;
+    }
+
+    printf("Todos los tests pasaron correctamente\n");
+    return 0;
 }
