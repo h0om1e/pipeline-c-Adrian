e0d912dda1332e3a75191ae983dfaca4ed7245f7
Adrián José López Casas <8864115@estudiantes.edumelilla.es>
2026-03-03 17:54:34 +0100

    Provocar fallo en test_app.c

--- test_app.c
+++ test_app.c
@@ -4,7 +4,7 @@ int sumar(int a, int b);
 
 int main() {
     assert(sumar(2, 3) == 6);
-    assert(sumar(-1, 1) == 0);
-    assert(sumar(0, 0) == 0);
+    assert(sumar(-1, 1) == 1);
+    assert(sumar(0, 0) == 2);
     return 0;
 }
